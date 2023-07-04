bfe71b989ee04a688357efa95f7fde9907b43f19
Chris Towles <Chris.Towles@gmail.com>
2023-07-04 11:05:15 -0400

    chore: eslint and formating

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,59 +1,60 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 import { appDescription } from './constants/index';
 
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    // css: ['@/assets/custom.scss'],
-    modules: ['nuxt-quasar-ui', '@vueuse/nuxt', '@pinia/nuxt', '@nuxtjs/color-mode', '@vite-pwa/nuxt'],
-    experimental: {
-        // when using generate, payload js assets included in sw precache manifest
-        // but missing on offline, disabling extraction it until fixed
-        //payloadExtraction: false,
-        //inlineSSRStyles: false,
-        //renderJsonPayloads: true,
-        typedPages: true,
-    },
-    quasar: {
-        plugins: ['BottomSheet', 'Dialog', 'Loading', 'LoadingBar', 'Notify', 'Dark'],
-        extras: {
-            font: 'roboto-font',
-        },
-        components: {
-            defaults: {
-                QBtn: {
-                    unelevated: true,
-                },
-            },
-        },
-    },
+  devtools: { enabled: true },
+  // css: ['@/assets/custom.scss'],
+  modules: [
+    'nuxt-quasar-ui', '@vueuse/nuxt', '@pinia/nuxt', '@nuxtjs/color-mode',
+  ],
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    // payloadExtraction: false,
+    // inlineSSRStyles: false,
+    // renderJsonPayloads: true,
+    typedPages: true,
+  },
 
-    nitro: {
-        esbuild: {
-            options: {
-                target: 'esnext',
-            },
-        },
-        prerender: {
-            crawlLinks: false,
-            routes: ['/'],
-            // ignore: ['/hi'],
-        },
+  quasar: {
+    plugins: ['BottomSheet', 'Dialog', 'Loading', 'LoadingBar', 'Notify', 'Dark'],
+    extras: {
+      font: 'roboto-font',
     },
+    components: {
+      defaults: {
+        QBtn: {
+          unelevated: true,
+        },
+      },
+    },
+  },
 
-    app: {
-        head: {
-            viewport: 'width=device-width,initial-scale=1',
-            link: [
-                { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-                { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-                { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
-            ],
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'description', content: appDescription },
-                { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-            ],
-        },
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
     },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      // ignore: ['/hi'],
+    },
+  },
+
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: appDescription },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
+  },
 });
